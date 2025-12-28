3c42f9478c90258598f6413dc90b4e86713ae4f4
Zakia <122170888+zakiakaidzan@users.noreply.github.com>
2025-12-29 03:52:15 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+from idlix_api import get_m3u8_by_id
+
+def handler(request):
+    video_id = request.args.get("id")
+    content_type = request.args.get("type", "movie").lower()
+
+    if content_type not in ["movie", "tv"]:
+        content_type = "movie"
+
+    # health check
+    if not video_id:
+        return {
+            "statusCode": 200,
+            "body": {
+                "status": True,
+                "message": "IDLIX M3U8 API is running"
+            }
+        }
+
+    result = get_m3u8_by_id(video_id, content_type)
+
+    return {
+        "statusCode": 200,
+        "body": result
+    }
