d9f8d2745a3211cd931a32d45585bd2d94bc7aa8
Zakia <122170888+zakiakaidzan@users.noreply.github.com>
2025-12-29 03:37:48 +0700

    Update idlix.py

--- api/idlix.py
+++ api/idlix.py
@@ -1,14 +1,13 @@
 from idlix_api import get_m3u8_by_id
 
 def handler(request):
+    # health check
     video_id = request.args.get("id")
     content_type = request.args.get("type", "movie").lower()
 
-    # validasi type
     if content_type not in ["movie", "tv"]:
         content_type = "movie"
 
-    # ✅ HEALTH CHECK (API SUDAH JALAN)
     if not video_id:
         return {
             "statusCode": 200,
@@ -18,7 +17,6 @@ def handler(request):
             }
         }
 
-    # 🔥 MAIN LOGIC
     result = get_m3u8_by_id(video_id, content_type)
 
     return {
